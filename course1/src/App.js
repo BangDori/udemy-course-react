a35ef34b56f1ed237af35e6e314a22ba870af28e
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-04-19 22:58:14 +0900

    비용 계산기 |

---
+++ course1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Expenses from "./components/Expenses";
+
+const App = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Cost Calculate</h2>
+      <Expenses />
+    </div>
+  );
+};
+
+export default App;
