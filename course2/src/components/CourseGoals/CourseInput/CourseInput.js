2fe19ca4d7d04dd42af89b60cf9be6641d629931
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-05-04 17:20:37 +0900

    입력창 - 동적 스타일링

---
+++ course2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const goalValueRef = useRef(null);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+    goalValueRef.current.focus();
+  };
+
+  console.log(isValid);
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid && "invalid"}`}>
+        <label>Course Goal</label>
+        <input
+          ref={goalValueRef}
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
