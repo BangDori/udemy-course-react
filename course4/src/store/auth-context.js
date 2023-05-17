1ae823e2247eb92e03054e1b1642d91e3acbccbd
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-05-17 11:24:01 +0900

    로그인 - useContext

---
+++ course4/src/store/auth-context.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {}, // IDE 자동 완성 기능에 도움
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation) setIsLoggedIn(true);
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
