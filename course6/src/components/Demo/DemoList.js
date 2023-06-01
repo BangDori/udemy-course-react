56e794d3890fe8523676df940e3c6b7d63c34f66
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-06-01 12:05:22 +0900

    Memoization using useMemo()

---
+++ course6/src/components/Demo/DemoList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import classes from "./DemoList.module.css";
+
+const DemoList = ({ title, items }) => {
+  console.log("DemoList RUNNING");
+
+  return (
+    <div className={classes.list}>
+      <h2>{title}</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(DemoList);
