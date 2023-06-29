e3ccf940a9ecd6a655fdec5603d9301bb0936f36
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-06-29 12:17:36 +0900

    Section 18: 리덕스에 뛰어들기 (컨텍스트 API의 대안) l

---
+++ course13/src/components/Counter.js
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
+import { INCREMENT, INCREASE, DECREMENT, TOGGLE } from "../store";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: INCREMENT });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: INCREASE, amount: 5 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: DECREMENT });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: TOGGLE });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment By 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
