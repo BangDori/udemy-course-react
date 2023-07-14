2a07cc6d129315af1ddfd7e5fc03856424048e33
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-07-14 12:51:49 +0900

    Section 20 l

---
+++ course15/src/pages/Home.js
@@ -0,0 +1,21 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of product</Link>
+      </p>
+      <button onClick={navigateHandler}>Navigate</button>
+    </>
+  );
+};
+
+export default HomePage;
