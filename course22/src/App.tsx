17d8c261109b859b7f299903186bb206fb65c9c3
BangDori <qudwns8865@naver.com>
2023-07-30 14:40:21 +0900

    Section 27

--- course22/src/App.tsx
+++ course22/src/App.tsx
@@ -1,28 +1,13 @@
-import { useState } from "react";
-
-import Todos from "./components/Todos";
-import Todo from "./models/todo";
-import "./App.css";
 import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import TodosContextProvider from "./store/todos-context";
 
 const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (text: string) => {
-    const newTodo = new Todo(text);
-
-    setTodos((prevTodos) => prevTodos.concat(newTodo));
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 };
 
